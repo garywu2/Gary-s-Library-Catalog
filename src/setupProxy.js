2e1b29f26ca3a52cebcb741da9332098556cb9a7
unknown <gary.wu2@ucalgary.ca>
2020-07-13 21:01:49 -0600

    Adding Proxy

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,9 +1,10 @@
+import config from "./config" 
 const { createProxyMiddleware } = require('http-proxy-middleware');
 module.exports = function(app) {
   app.use(
     '/catalog',
     createProxyMiddleware({
-      target: 'http://localhost:5000',
+      target: 'http://ec2-3-21-113-3.us-east-2.compute.amazonaws.com:5000',
       changeOrigin: true,
     })
   );
